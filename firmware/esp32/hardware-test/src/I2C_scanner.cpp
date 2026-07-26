f4f952ad38524e27443ba1ac4281167ac7a96490
dageb <dagebg.dev@gmail.com>
2026-07-26 11:40:46 +0200

    changed wiring, accidentally wired encoder with boot pin which caused problems

--- firmware/esp32/hardware-test/src/I2C_scanner.cpp
+++ firmware/esp32/hardware-test/src/I2C_scanner.cpp
@@ -29,5 +29,23 @@ void setup() {
 }
 
 void loop() {
-  // Nothing to do here
+  for (byte addr = 1; addr < 127; addr++ ) {
+      Wire.beginTransmission(addr);
+      byte error = Wire.endTransmission();
+
+      if (error == 0) {
+        Serial.print("I2C device found at address 0x");
+        if (addr < 16) {
+          Serial.print("0");
+        }
+        Serial.println(addr, HEX);
+      } else if (error == 4) {
+        Serial.print("Unknown error at address 0x");
+        if (addr < 16) {
+          Serial.print("0");
+        }
+        Serial.println(addr, HEX);
+      }
+    }
+  delay(5000);
 }
